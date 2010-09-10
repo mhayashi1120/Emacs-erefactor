077414549cccf207c85df5e788b76a2e969453e3
Hayashi Masahiro <mhayashi1120@gmail.com>
2010-09-10 22:02:40 +0900

    fix url

--- el-tools.el
+++ el-tools.el
@@ -2,7 +2,7 @@
 
 ;; Author: Hayashi Masahiro <mhayashi1120@gmail.com>
 ;; Keywords: lisp refactor lint
-;; URL: TODO
+;; URL: http://gist.github.com/573547.txt
 ;; Emacs: GNU Emacs 22 or later
 
 ;; This program is free software; you can redistribute it and/or
