4c3fb8c317e4bab062b542f964633a18811fd131
Hayashi Masahiro <mhayashi1120@gmail.com>
2011-03-02 22:40:09 +0900

    Version up

--- erefactor.el
+++ erefactor.el
@@ -5,7 +5,7 @@
 ;; URL: http://github.com/mhayashi1120/Emacs-erefactor/raw/master/erefactor.el
 ;; URL: http://www.emacswiki.org/emacs/download/erefactor.el
 ;; Emacs: GNU Emacs 22 or later
-;; Version: 0.5.0
+;; Version: 0.5.1
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
